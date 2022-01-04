447c7ddfe2191012c3415d4cc463a38f41f3bfc6
bale.kumar <vinay0639>
2022-01-04 16:50:16 +0530

    comitt : objectTest : checkTest : CarObject

---
+++ day-2/java-simple-project/src/main/java/clsobj/Car.java
@@ -0,0 +1,15 @@
+package clsobj;
+
+public class Car {
+    //state
+    public int speed;
+
+    //default
+    public Car()
+    {speed = 10;}
+
+    //parameterised
+    public Car(int sp){
+        speed = sp;
+    }
+}
---
+++ day-2/java-simple-project/src/test/java/CarTest.java
@@ -0,0 +1,42 @@
+import clsobj.Car;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+
+public class CarTest {
+    @DisplayName("Car Invalid Object")
+    @Test
+    void testCarInvalidObject(){
+        Car car=null;
+        Assertions.assertNull(car);
+    }
+
+    @DisplayName("Car valid Object")
+    @Test
+    void testCarValidObject(){
+        Car car=new Car();
+
+        Assertions.assertNotNull(car);
+    }
+    @DisplayName("Car Speed By Default Constructor")
+    @Test
+    void testCarSpeedDefault(){
+        Car car=new Car();
+
+        int expectedSpeed=10;
+        int actualSpeed = car.speed;
+
+        Assertions.assertEquals(expectedSpeed,actualSpeed);
+    }
+    @DisplayName("Car Speed By Parameterised Constructor")
+    @Test
+    void testCarSpeedParameterised(){
+        Car car=new Car(100);
+
+        int expectedSpeed = 100;
+        int actualSpeed = car.speed;
+
+        Assertions.assertEquals(expectedSpeed,actualSpeed);
+    }
+}
