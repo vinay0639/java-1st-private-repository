a415f5d0748cd5494039b222215f66beb9c5aeaa
bale.kumar <vinay0639>
2022-01-07 16:36:09 +0530

    commiting springs

---
+++ day-5/spring-basic-app/src/main/java/Main.java
@@ -0,0 +1,15 @@
+import car.Car;
+import car.Driver;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car car = new Car();
+
+        // Driver driver = new Driver(car);
+
+        Driver driver = new Driver();
+        driver.setCar(car);
+    }
+}
