65d4880a214a58eb6178081f6930a40a428fc7aa
bale.kumar <vinay0639>
2022-01-10 14:57:35 +0530

    annotation : before and after

--- week-2/day-1/spring-app-annotation-based/src/main/java/com/ani/logging/CarLogger.java
+++ week-2/day-1/spring-app-annotation-based/src/main/java/com/ani/logging/CarLogger.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class CarLogger {
+    @Pointcut("execution(public * com.ani.car.Car.speedUp(..))")
+    public void pointCutAfterExecution(){ }
+
     @Before("execution(* com.ani.car.*.*(..))")
     public void logBeforeAnyMethodExecutionCarPackage(JoinPoint jp) {
         System.out.println("Before Execution I am getting printed");
@@ -16,4 +19,9 @@ public class CarLogger {
         System.out.println("After this line you will see method output");
         System.out.println("----------------");
     }
+    @After("pointCutAfterExecution()")
+    public void logAfterAnyMethods(JoinPoint jp){
+        System.out.println("After Execution iam getting printed");
+    }
+
 }
