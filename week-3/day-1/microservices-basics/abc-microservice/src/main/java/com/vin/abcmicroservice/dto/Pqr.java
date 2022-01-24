430e1102aaaf9c06555590c75b57fe0850a7b4a7
bale.kumar <vinay0639>
2022-01-24 13:27:11 +0530

    commiting - abc-microservices

---
+++ week-3/day-1/microservices-basics/abc-microservice/src/main/java/com/vin/abcmicroservice/dto/Pqr.java
@@ -0,0 +1,15 @@
+package com.vin.abcmicroservice.dto;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@Data
+public class Pqr {
+    private Long id;
+    private String name;
+    private LocalDate date;
+}
---
+++ week-3/day-1/microservices-basics/abc-microservice/src/test/java/com/vin/abcmicroservice/AbcMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vin.abcmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
